8ca9ff93f9ddfe9384997afbb47a687e28155675
Fux, Etienne <etienne.fux@zuehlke.com>
2018-10-10 09:11:31 +0200

    Add skeleton for spring application

---
+++ src/main/java/ch/zuehlke/bibweb/BibwebApplication.java
@@ -0,0 +1,12 @@
+package ch.zuehlke.bibweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibwebApplication.class, args);
+	}
+}
