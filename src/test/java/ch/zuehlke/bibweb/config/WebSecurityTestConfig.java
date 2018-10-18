0fbeb2268bd78e853fd36baa5acec29936a87dc2
Stefan Friedli <stefan.friedli@zuehlke.com>
2018-10-18 14:56:29 +0200

    update test security configuration

--- src/test/java/ch/zuehlke/bibweb/config/WebSecurityTestConfig.java
+++ src/test/java/ch/zuehlke/bibweb/config/WebSecurityTestConfig.java
@@ -4,6 +4,7 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
@@ -13,6 +14,9 @@ public class WebSecurityTestConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
                 .authorizeRequests()
                 .antMatchers("/token/**").permitAll()
                 .antMatchers("/**").authenticated();
