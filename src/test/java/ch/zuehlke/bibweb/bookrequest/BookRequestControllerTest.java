d50e36a18dd8e48ead04fcf913017b5307638c9b
Fux, Etienne <etienne.fux@zuehlke.com>
2018-10-18 13:48:16 +0200

    Create web security config for tests

--- src/test/java/ch/zuehlke/bibweb/bookrequest/BookRequestControllerTest.java
+++ src/test/java/ch/zuehlke/bibweb/bookrequest/BookRequestControllerTest.java
@@ -1,28 +1,18 @@
 package ch.zuehlke.bibweb.bookrequest;
 
-import ch.zuehlke.bibweb.user.User;
-import ch.zuehlke.bibweb.user.UserRepository;
-import ch.zuehlke.bibweb.user.UserServiceImpl;
+import ch.zuehlke.bibweb.config.WebSecurityTestConfig;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.Bean;
 import org.springframework.http.MediaType;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.test.context.support.WithAnonymousUser;
 import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 
 import java.util.Arrays;
 import java.util.List;
@@ -33,17 +23,15 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.doNothing;
-import static org.mockito.Mockito.when;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
-import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(BookRequestController.class)
-@ContextConfiguration
+@ContextConfiguration(classes = {WebSecurityTestConfig.class, BookRequestController.class})
 @WebAppConfiguration
 public class BookRequestControllerTest {
 
@@ -57,17 +45,6 @@ public class BookRequestControllerTest {
             new BookRequest((long) 1, "123", "user1"),
             new BookRequest((long) 2, "456", "user2")
     );
-
-    @Autowired
-    private WebApplicationContext context;
-
-    @Before
-    public void setup() {
-        mvc = MockMvcBuilders
-                .webAppContextSetup(context)
-                .apply(springSecurity())
-                .build();
-    }
 
     @Test
     public void getAllBookRequests() throws Exception {
@@ -125,7 +102,7 @@ public class BookRequestControllerTest {
     }
 
     @Test
-    @WithMockUser(username = "stefan", roles = "")
+    @WithMockUser(username = "stefan", roles = "ADMIN")
     public void updateBookRequest() throws Exception{
         final BookRequest bookRequest = new BookRequest("3932420942092", "testuser", BookRequestState.ACCEPTED);
 
---
+++ src/test/java/ch/zuehlke/bibweb/config/WebSecurityTestConfig.java
@@ -0,0 +1,21 @@
+package ch.zuehlke.bibweb.config;
+
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class WebSecurityTestConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/token/**").permitAll()
+                .antMatchers("/**").authenticated();
+    }
+
+}
