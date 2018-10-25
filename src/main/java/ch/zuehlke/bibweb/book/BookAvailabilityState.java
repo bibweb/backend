99a3a1ae06127b6e6c833f53f2d29a7bc9c40c25
Fux, Etienne <etienne.fux@zuehlke.com>
2018-10-25 09:21:12 +0200

    Encode availability state as int in DTO

--- src/main/java/ch/zuehlke/bibweb/book/BookAvailabilityState.java
+++ src/main/java/ch/zuehlke/bibweb/book/BookAvailabilityState.java
@@ -1,5 +1,8 @@
 package ch.zuehlke.bibweb.book;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+@JsonFormat(shape = JsonFormat.Shape.NUMBER_INT)
 public enum BookAvailabilityState {
     AVAILABLE,
     UNAVAILABLE,
