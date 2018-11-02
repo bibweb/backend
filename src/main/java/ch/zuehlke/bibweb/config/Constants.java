121f5ed8c6179c4e54076cdd5e34328a653595f9
Fux, Etienne <etienne.fux@zuehlke.com>
2018-11-02 08:45:25 +0100

    Calculate duration in seconds for token validity

--- src/main/java/ch/zuehlke/bibweb/config/Constants.java
+++ src/main/java/ch/zuehlke/bibweb/config/Constants.java
@@ -1,7 +1,7 @@
 package ch.zuehlke.bibweb.config;
 
 public class Constants {
-    public static final long ACCESS_TOKEN_VALIDITY_SECONDS = 2*60*60;
+    public static final long ACCESS_TOKEN_VALIDITY_SECONDS = 18000;
     public static final String TOKEN_PREFIX = "Bearer ";
     public static final String HEADER_STRING = "Authorization";
     public static final String AUTHORITIES_KEY = "scopes";
